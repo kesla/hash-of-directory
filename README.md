ba9f089c44a63f5fcb8db85ea882b0e86c3b3d4b
David Björklund <david.bjorklund@gmail.com>
2023-04-01 20:56:46 +0200

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# hash-of-directory
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run lib/index.ts
+```
+
+This project was created using `bun init` in bun v0.5.8. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { BinaryToTextEncoding, createHash, Hash } from "crypto";
+import { readdir, readFile } from "fs/promises";
+
+const calculate = async (dir: string, hash: Hash) => {
+  const files = (await readdir(dir, { withFileTypes: true })).sort();
+  for (const file of files) {
+    if (file.isDirectory()) {
+      hash.update(file.name);
+      await calculate(`${dir}/${file.name}`, hash);
+    } else {
+      const content = await readFile(`${dir}/${file.name}`);
+
+      hash.update(file.name);
+      hash.update(content);
+    }
+  }
+};
+
+export const hashOfDirectory = async (dir: string, encoding: BinaryToTextEncoding = 'base64') => {
+  const hash = createHash("sha256");
+  await calculate(dir, hash);
+  return hash.digest(encoding);
+};
